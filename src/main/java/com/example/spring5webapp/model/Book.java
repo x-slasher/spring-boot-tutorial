062c774c42200377f3dc0e91b3628d659610c45f
nayeem <nayeem.hasanewu@gmail.com>
2021-06-27 16:35:14 +0600

    test commit 1

--- src/main/java/com/example/spring5webapp/model/Book.java
+++ src/main/java/com/example/spring5webapp/model/Book.java
@@ -19,6 +19,7 @@ public class Book {
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
     inverseJoinColumns = @JoinColumn(name = "author_id"))
+
     private Set<Author> authors = new HashSet<>();
 
     public Book(){
